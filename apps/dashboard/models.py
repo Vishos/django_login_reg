4d3b54c223dcd1d42ac3c676897c18b9e41fc402
Vishos <graham.layman@comcast.net>
2018-01-31 14:18:01 -0800

    added createPoke

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -4,9 +4,27 @@ from django.db import models
 from ..login.models import User
 # Create your models here.
 
+class PokeManager(models.Manager):
+	def createPoke(self,sender, receiver):
+		sender = User.objects.get(id=sender)
+		receiver = User.objects.get(id=receiver)
+		print "***********"
+		print "inside pokeManager about to create a poke"
+		print "************"
+		existingPoke = Poke.objects.filter(poke_sender=sender, poke_receiver=receiver)
+		if len(existingPoke) > 0:
+			existingPoke[0].count += 1
+			existingPoke[0].save()
+		else:
+			Poke.objects.create(poke_sender=sender, poke_receiver=receiver)
+		return "done"
+    	
+
 class Poke(models.Model):
 	poke_sender = models.ForeignKey(User, related_name="pokes_sent")
 	poke_receiver = models.ForeignKey(User, related_name="pokes_received")
 	count = models.IntegerField(default = 1)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	objects = PokeManager()
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, reverse, redirect
 from ..login.models import User
+from .models import Poke
 # Create your views here.
 def landing(request):
 	context = {
-		'all_users' : User.objects.all()
+		'all_users' : User.objects.all(),
+		'all_pokes' : Poke.objects.all()
 	}
 	return render(request, 'dashboard/landing.html', context)
 
 def createPoke(request):
 	print request.POST['poke_receiver']
+	Poke.objects.createPoke(
+		sender=request.session['user_id'], 
+		receiver=request.POST['poke_receiver'])
 	return redirect('/dashboard')
