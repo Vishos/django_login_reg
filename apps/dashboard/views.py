2c78bf9dd0debce98f56438bf451d7ad4131b2ce
Vishos <graham.layman@comcast.net>
2018-01-31 15:35:22 -0800

    final commit

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, reverse, redirect
 from ..login.models import User
+from django.db.models import Sum
 from .models import Poke
 # Create your views here.
 def landing(request):
+    	user = User.objects.get(id=request.session['user_id'])
 	context = {
-		'all_users' : User.objects.all(),
-		'all_pokes' : Poke.objects.all()
+		'session_user' : user,
+		'all_users' : User.objects.exclude(id=request.session['user_id']).annotate(num_pokes=Sum('pokes_received__count')),
+		'all_pokes' : Poke.objects.all(),
+		'frienemies': Poke.objects.filter(poke_receiver=user).order_by("-count")
 	}
 	return render(request, 'dashboard/landing.html', context)
 
